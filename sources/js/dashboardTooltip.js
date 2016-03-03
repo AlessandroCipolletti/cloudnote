c552e84f3bcfe2662b0f4fbf6f016b63441f31c3
Alessandro Cipolletti <a.cipolletti@webpublication.fr>
2016-03-03 11:49:39 +0100

    Dashboard Tooltip preview image centered

--- sources/js/dashboardTooltip.js
+++ sources/js/dashboardTooltip.js
@@ -7,6 +7,29 @@
   var _container = {}, _overlay = {}, _tooltip = {}, _previewImage = new Image();
   var _selectedId = 0;
 
+  function show (draw) {
+
+    if (draw) {
+
+      _selectedId = draw.id;
+
+      _previewImage.src = draw.data.getAttributeNS("http://www.w3.org/1999/xlink", "href");
+
+      app.Utils.fadeInElements(_overlay);
+      _tooltip.classList.add("cloudnote-dashboard-tooltip__panel-visible");
+
+    }
+
+  }
+
+  function _hide () {
+
+    _selectedId = 0;
+    app.Utils.fadeOutElements(_overlay);
+    _tooltip.classList.remove("cloudnote-dashboard-tooltip__panel-visible");
+
+  }
+
   function _onRotate (e) {
     // do some stuff
   }
@@ -23,37 +46,14 @@
     _overlay.addEventListener(app.Param.eventStart, _hide);
     _tooltip = app.Utils.createDom("cloudnote-dashboard-tooltip__panel");
 
+    var previewCont = app.Utils.createDom("cloudnote-dashboard-tooltip__preview-container");
     _previewImage.classList.add("cloudnote-dashboard-tooltip__preview");
-    _tooltip.appendChild(_previewImage);
+    previewCont.appendChild(_previewImage);
+    _tooltip.appendChild(previewCont);
 
     container.appendChild(_overlay);
     container.appendChild(_tooltip);
     app.Main.addRotationHandler(_onRotate);
-
-  }
-
-  function show (draw) {
-
-    if (draw) {
-
-      _selectedId = draw.id;
-
-      _previewImage.src = draw.data.getAttributeNS("http://www.w3.org/1999/xlink", "href");
-      _previewImage.style.marginTop = "calc(62% - " + _previewImage.height + "px)";
-      _previewImage.style.marginLeft = "calc(50% - " + (_previewImage.width / 2) + "px)";
-
-      app.Utils.fadeInElements(_overlay);
-      _tooltip.classList.add("cloudnote-dashboard-tooltip__panel-visible");
-
-    }
-
-  }
-
-  function _hide () {
-
-    _selectedId = 0;
-    app.Utils.fadeOutElements(_overlay);
-    _tooltip.classList.remove("cloudnote-dashboard-tooltip__panel-visible");
 
   }
 
