050f10d747a6db1fc0ca19505a76cdcd432f1903
Alessandro Cipolletti <cipolletti.alessandro@gmail.com>
2016-03-23 02:38:56 +0100

    Fix editor line

--- sources/js/editor.js
+++ sources/js/editor.js
@@ -12,7 +12,7 @@
 
   var _config = {
     primaryColors: ["#000000", "#808080", "#C0C0C0", "#6DF4FF", "#007AFF", "#0000FF", "#800080", "#000080", "#FFFF00", "#00FF00", "#4CD900", "#A08066","#F06A31", "#008000", "#FF0000", "#A52A2A", "#800000"],
-    tools: ["marker", "pencil", "eraser", "undo", "redo", "save", "paper", "clear", "exit"],
+    tools: ["marker", "pencil", "eraser", "undo", "redo", "save", "clear", "paper", "exit"],
     toolsSide: "left"
   };
 
@@ -410,10 +410,12 @@
   }
 
   function _onGestureStart (e) {
+    _touchDown = false;
     console.log(e);
   }
 
   function _onGestureChange (e) {
+    _touchDown = false;
     console.log(e);
   }
 
